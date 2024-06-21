99894b9d1d322cf86e4bc643042507e92874a07c
Lakhdar Khl <82035622+LakhdarKhl@users.noreply.github.com>
2024-06-21 14:20:29 +0200

    json add fake api

--- cypress.config.js
+++ cypress.config.js
@@ -7,3 +7,14 @@ module.exports = defineConfig({
     },
   },
 });
+
+const { defineConfig } = require("cypress");
+module.exports = defineConfig({
+ e2e: {
+ baseUrl: 'http://localhost:3000',
+ excludeSpecPattern: '**/examples/*.cy.js',
+ setupNodeEvents(on, config) {
+ // implement node event listeners here
+ },
+ },
+});
